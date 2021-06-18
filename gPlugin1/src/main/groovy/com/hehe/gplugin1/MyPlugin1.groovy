7169620ad002a10936c28da4603fed25d6a73322
wangwei <wangwei@zhiping.tech>
2021-06-18 17:57:57 +0800

    ASM

---
+++ gPlugin1/src/main/groovy/com/hehe/gplugin1/MyPlugin1.groovy
@@ -0,0 +1,20 @@
+package com.hehe.gplugin1
+
+import com.android.build.gradle.AppExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class MyPlugin1 implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        println("----------- this is my plugin1 ----------")
+
+        def android = project.extensions.getByType(AppExtension)
+        println '----------- registering AutoTrackTransform  -----------'
+        LifeCycleTransform transform = new LifeCycleTransform()
+        android.registerTransform(transform)
+
+    }
+}
+
+
