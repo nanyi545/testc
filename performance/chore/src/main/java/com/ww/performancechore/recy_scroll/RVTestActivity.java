369895d6140c0b2e49e68e7d84de39348bd5cec6
ww <nanyi545@126.com>
2021-11-20 23:09:10 +0800

    invalidate和requestLayout

--- performance/chore/src/main/java/com/ww/performancechore/recy_scroll/RVTestActivity.java
+++ performance/chore/src/main/java/com/ww/performancechore/recy_scroll/RVTestActivity.java
@@ -45,6 +45,20 @@ public class RVTestActivity extends AppCompatActivity {
      *
      */
 
+    /**
+     * https://www.jianshu.com/p/b6437e4b81ac
+     *
+     * invalidate()和requestLayout()
+     *我们已经分析过了invalidate()和requestLayout()的具体实现，现在就来总结一下invalidate()和requestLayout()的异同：
+     * 相同点
+     * 1.invalidate()和requestLayout()方法最终都会调用ViewRootImpl的performTraversals()方法。
+     * 不同点
+     * 1.invalidate()方法不会执行measureHierarchy()和performLayout()方法，也就不会执行measure和layout流程，只执行draw流程，如果开启了硬件加速则只进行调用者View的重绘。
+     * 2.requestLayout()方法会依次measureHierarchy()、performLayout()和performDraw()方法，调用者View和它的父级View会重新进行measure、layout，一般情况下不会执行draw流程，子View不一定会重新measure和layout。
+     * 综上，当只需要进行重新绘制时就调用invalidate()，如果需要重新测量和布局就调用requestLayout()，但是requestLayout()不保证进行重新绘制，如果要进行重新绘制可以再手动调用invalidate()。
+     *
+     */
+
     VG2 vg1;
     V1 v1;
     private void test1(){
