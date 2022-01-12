2c45341167a55de51311d14763daf465568d7f75
wangwei <wangwei@zhiping.tech>
2022-01-12 19:08:51 +0800

    question

---
+++ app/src/test/java/com/example/testc2/string/Substring1.java
@@ -0,0 +1,19 @@
+package com.example.testc2.string;
+
+
+import java.util.List;
+
+/**
+ *
+ * You are given a string s and an array of strings words of the same length. Return all starting indices of substring(s) in s that is a concatenation of
+ * each word in words exactly once, in any order, and without any intervening characters.
+ **
+ * https://leetcode-cn.com/problems/substring-with-concatenation-of-all-words/
+ */
+public class Substring1 {
+
+    public List<Integer> findSubstring(String s, String[] words) {
+        return null;
+    }
+    
+}
