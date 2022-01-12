56701ccacd40231f596d3e413f88d8b38c13d29a
wangwei <wangwei@zhiping.tech>
2022-01-12 20:01:56 +0800

    question

--- app/src/test/java/com/example/testc2/string/Substring1.java
+++ app/src/test/java/com/example/testc2/string/Substring1.java
@@ -3,7 +3,10 @@ package com.example.testc2.string;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
@@ -22,11 +25,31 @@ public class Substring1 {
         String[] words = {"foo","bar"};
         List<Integer> a = findSubstring(s, words);
         // 0,9
+        int ind1 =s.indexOf("foo");
+//        System.out.println("ind:"+ind1);
+//        System.out.println("substring:"+s.substring(0,2));  //  ba
+
     }
 
 
 
     public List<Integer> findSubstring(String s, String[] words) {
+        int totalSize = s.length();
+        int wSize = words.length;
+        int wordLength = words[0].length();
+        Map<String,List<Integer>> record = new HashMap<>();
+
+        for (int i=0;i<wSize;i++){
+            List<Integer> indexes = new ArrayList<>();
+            String word = words[i];
+            for (int j=0;j<totalSize-wordLength;j++){
+                String t = s.substring(j,j+wordLength);
+                if(t.equals(word)){
+                    indexes.add(j);
+                }
+            }
+            System.out.println("w:"+word+"  pos:"+indexes);
+        }
         return null;
     }
 
