3ccc2bd822c9eef5741cef7e6c0d75e021d28d0b
wangwei <wangwei@zhiping.tech>
2022-02-18 17:34:21 +0800

    cpp make file

---
+++ cpp/native1/src/main/cpp/leet/c1791.c
@@ -0,0 +1,13 @@
+//
+// Created by wei wang on 2022-02-18.
+//
+//   https://leetcode-cn.com/problems/find-center-of-star-graph/
+
+
+#include <stdio.h>
+
+int main() {
+    printf("1+2=%d\n");
+    return 0;
+}
+
