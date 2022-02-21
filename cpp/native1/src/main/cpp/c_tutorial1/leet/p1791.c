6dd4dd96a9a82dc3a371d15e6e8a2ed389e8d194
wangwei <wangwei@zhiping.tech>
2022-02-21 16:57:05 +0800

    cpp

--- cpp/native1/src/main/cpp/c_tutorial1/leet/p1791.c
+++ cpp/native1/src/main/cpp/c_tutorial1/leet/p1791.c
@@ -21,31 +21,66 @@ int findCenter(int** edges, int edgesSize, int* edgesColSize){
             }
         }
     }
-    int* arr = malloc(sizeof(int)*max);
-    for (int i=0;i<edgesSize;i++){
-        arr[i] = 0;
-    }
-    for (int i=0;i<edgesSize;i++){
-        for(int j=0;j<*edgesColSize;j++){
-            int t = *(*(edges+i)+j);
-            arr[t]++;
-        }
-    }
-
-    for (int i=0;i<edgesSize;i++){
-        for(int j=0;j<*edgesColSize;j++){
-            int t = *(*(edges+i)+j);
-            if(arr[t]==edgesSize){
-                return arr[t];
-            }
-        }
-    }
+    printf("max:%d \n",max);
+//    int* arr = malloc(sizeof(int)*max);
+//    for (int i=0;i<edgesSize;i++){
+//        arr[i] = 0;
+//    }
+//    for (int i=0;i<edgesSize;i++){
+//        for(int j=0;j<*edgesColSize;j++){
+//            int t = *(*(edges+i)+j);
+//            arr[t]++;
+//        }
+//    }
+//
+//    for (int i=0;i<edgesSize;i++){
+//        for(int j=0;j<*edgesColSize;j++){
+//            int t = *(*(edges+i)+j);
+//            if(arr[t]==edgesSize){
+//                return arr[t];
+//            }
+//        }
+//    }
 
     return -1;
 }
 
 int main() {
-    printf("1+2\n");
+    int arr[3][2]={{1,2},{3,2},{0,2}};
+    int s = 2;
+
+    // 1d array
+//    int arr1[3]={1,2,3};
+//    int *p1 = arr1;
+//    printf("p:%x\n",arr1);
+//    printf("p:%x\n",p1);
+//    printf("p0:%x\n",&arr1[0]);
+//    printf("p1:%x\n",&arr1[1]);
+//    printf("p2:%x\n",&arr1[2]);
+
+    // 2d array
+//    int **p2 = NULL;
+//    int *p1_ = NULL;
+//    for (int i=0;i<3;i++){
+//        for (int j=0;j<2;j++){
+//            p1_ = arr[i];
+//            p2 = &p1_;
+//            printf("i:%d,j:%d,v:%d\n",i,j,*(*(p2)+j));
+//        }
+//    }
+
+    int** pp = &arr[0];
+
+
+
+    printf("p0-0:%x\n",*(*(&arr[0])+0));
+    printf("p0-1:%x\n",*(*(&arr[0])+1));
+
+    printf("p1:%x\n",&arr[1]);
+    printf("p2:%x\n",&arr[2]);
+
+
+//    findCenter( &arr[0] ,3, &s);
     return 0;
 }
 
---
+++ cpp/native1/src/main/cpp/cpp_1/basic/b1.cpp
@@ -0,0 +1,41 @@
+//
+// Created by wei wang on 2022-02-21.
+//
+// g++ b1.cpp -o b1
+// g++ -std=c++11 -o b1 b1.cpp
+// g++ -Wall -Werror -pedantic -std=c++11 -o b1 b1.cpp
+
+#include <iostream>
+#include <string>
+using std::cout;
+using std::endl;
+using std::string;
+const char newline = '\n';
+#define NEWLINE '\n'
+
+int main()
+{
+    cout << "---Hello World!\n";
+
+    int a=5;       // initial value: 5
+    int b(3);      // initial value: 3
+    int c {2};     // initial value: 2
+    int result;    // initial value undetermined
+    a = a + b;
+    result = a - c;
+    cout << result << "\n";
+
+
+    int foo = 0;
+    auto bar = foo;  // the same as: int bar = foo;
+    bar  = 99;
+    cout << bar << newline;
+
+
+    string mystring;
+    mystring = "This is the initial string content";
+    cout << mystring << endl;
+    mystring = "This is a different string content";
+    cout << mystring << NEWLINE;
+
+}
